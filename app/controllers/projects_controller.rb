0edef3f015bdc09e9a4064a41a976fb003fd3abd
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2019-05-05 23:33:38 -0700

    Set up the show action in projects_controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,5 +15,15 @@ class ProjectsController < ApplicationController
 		else
 		# nothing yet
 		end	
-	end	
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	private	
+
+	def project_params
+		params.require(:project).permit(:name, :description)
+	end		
 end
