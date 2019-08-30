8859a6cc22145ae539fe331e87c186b38ebc85f3
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2019-08-29 22:54:17 -0700

    now the update field cant be blank -I dont care if it breaks the previous tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
+  validates :description, presence: true
 end
--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.feature "Users can edit existing projects" do
+
+
   before do
     FactoryGirl.create(:project, name: "Sublime Text 3")
 
@@ -11,6 +13,7 @@ RSpec.feature "Users can edit existing projects" do
 
   scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
+    fill_in "Description", with: "A text editor for everyone"
     click_button "Update Project"
 
     expect(page).to have_content "Project has been updated."
@@ -18,10 +21,10 @@ RSpec.feature "Users can edit existing projects" do
   end
 
   scenario "when providing invalid attributes" do
-    fill_in "Name", with: ""
     click_button "Update Project"
 
     expect(page).to have_content "Project has not been updated."
+    expect(page).to have_content "Description can't be blank"
   end
 
 
