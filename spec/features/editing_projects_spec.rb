db51130c689d25766de7a9340eb2dfffad170534
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2019-08-29 20:11:35 -0700

    defined added update funtion in the controller

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,12 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "Users can edit existing projects" do
-  scenario "with valid attributes" do
+  before do
     FactoryGirl.create(:project, name: "Sublime Text 3")
 
     visit "/"
     click_link "Sublime Text 3"
     click_link "Edit Project"
+  end
+
+  scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
     click_button "Update Project"
 
