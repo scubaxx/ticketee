24ee88a840a6eeb9390935729ab7bb47ae653644
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2019-10-19 16:12:56 -0700

    DENIED ACCESS SS implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 	include Pundit
+
+	rescue_from Pundit::NotAuuthorizedError, with: :not_authorized
+
+	private
+
+	def not_authorized
+		redirect_to root_path, alert: "ACCESS DENIED."
+	end	
+
 end
